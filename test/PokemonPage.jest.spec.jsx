7b92711f988bf2f7fc71cb2256ab05938b9f5e69
Yousef <yousefsameh24@gmail.com>
2024-02-20 15:04:03 +0200

    Remove unused code and update test case in PokemonPage.jest.spec.jsx

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -71,18 +71,6 @@ const pokemonList = {
   sprites: { front_default: 'URL' }
 }
 
-const previous = {
-  url: 'https://pokeapi.co/api/v2/pokemon/132/',
-  name: 'ditto',
-  id: 132
-}
-
-const next = {
-  url: 'https://pokeapi.co/api/v2/pokemon/134/',
-  name: 'vaporeon',
-  id: 134
-}
-
 describe('<PokemonPage />', () => {
   it('should render abilities', async () => {
     axiosMock.get.mockResolvedValueOnce({ data: pokemonList })
@@ -141,5 +129,5 @@ describe('<PokemonPage />', () => {
 
     expect(screen.queryByText('Previous')).toBeNull()
     expect(screen.queryByText('Next')).toBeNull()
-  })  
+  })
 })

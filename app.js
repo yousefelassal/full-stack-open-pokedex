de0fb3cc1122f7ea519e7c87493c18e6ff9e72f8
Yousef <yousefsameh24@gmail.com>
2024-02-23 22:07:24 +0200

    Add version endpoint for testing deployment

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.use(express.static("dist"))
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})

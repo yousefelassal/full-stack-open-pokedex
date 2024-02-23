622598264d8487e46ab7fcb26e8e34f01116a01d
Yousef <yousefsameh24@gmail.com>
2024-02-23 22:25:11 +0200

    Add /health endpoint to app.js

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ app.listen(PORT, () => {
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
